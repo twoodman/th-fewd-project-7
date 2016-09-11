952d6c07a9e63fdcf2d4ef343a2e3c3b0f7b3f9b
twoodman <tdwoodman@outlook.com>
2016-09-11 08:18:14 -0600

    start on player functionality add play/pause add restart add beginning of hover to show button bar

---
+++ javascripts/player.js
@@ -0,0 +1,46 @@
+// iife
+(function() {
+  'use strict';
+  // store player and all buttons in consts
+  const videoPlayer = document.querySelector('.player');
+  const playerButtons = document.querySelector('.player__buttons');
+  const playerRestart = document.querySelector('.btn__restart');
+  const playerRewind = document.querySelector('.btn__rewind');
+  const playerPlay = document.querySelector('.btn__play');
+
+  // only show player controls on videoplayer hover
+  videoPlayer.addEventListener('mouseover', () => {
+    if(playerButtons.classList) {
+      playerButtons.classList.add('player__buttons--show');
+    } else {
+      playerButtons.classList += ' ' + 'player__buttons--show';
+    }
+  }, false);
+
+  videoPlayer.addEventListener('mouseleave', () => {
+    if(playerButtons.classList) {
+      playerButtons.classList.remove('player__buttons--show');
+    } else {
+      playerButtons.classList -= ' ' + 'player__buttons--show';
+    }
+  }, false);
+
+  // play & pause click event
+  playerPlay.addEventListener('click', () => {
+    if(videoPlayer.paused) {
+      videoPlayer.play();
+      playerPlay.textContent = '||';
+    } else {
+      videoPlayer.pause();
+      playerPlay.textContent = '>';
+    }
+  }, false);
+
+  // restart click event
+  playerRestart.addEventListener('click', () => {
+    videoPlayer.currentTime = 0;
+  },false);
+
+  // rewind event
+
+}());
