7c8e0fb6b84435037d15659ad1aec42609b698ac
twoodman <tdwoodman@outlook.com>
2016-09-12 22:06:51 -0600

    add custom seek bar, styles, functionality add custom seek bar thumb, styles, icon, functionality add mute button, styles, icon, functionality fix seek thumb jumping around as seeking add a few more comments in player.js

--- javascripts/player.js
+++ javascripts/player.js
@@ -1,38 +1,28 @@
 // iife
 (function () {
   'use strict'
-  // store player and all buttons in consts
+  // store player and all buttons/inputs in consts
   const videoPlayer = document.querySelector('.player')
   const playerRestart = document.querySelector('.btn__restart')
   const playerRewind = document.querySelector('.btn__rewind')
   const playerForward = document.querySelector('.btn__forward')
   const playerPlay = document.querySelector('.btn__play')
-
-  // // only show player controls on videoplayer hover
-  // videoPlayer.addEventListener('mouseover', () => {
-  //   if(playerButtons.classList) {
-  //     playerButtons.classList.add('player__buttons--show');
-  //   } else {
-  //     playerButtons.classList += ' ' + 'player__buttons--show';
-  //   }
-  // }, false);
-
-  // videoPlayer.addEventListener('mouseleave', () => {
-  //   if(playerButtons.classList) {
-  //     playerButtons.classList.remove('player__buttons--show');
-  //   } else {
-  //     playerButtons.classList -= ' ' + 'player__buttons--show';
-  //   }
-  // }, false);
+  const playerMute = document.querySelector('.btn__mute')
+  const playerBar = document.querySelector('.player__bar')
 
   // play function
   function playVideo () {
+    // if video paused already
     if (videoPlayer.paused) {
+      // play it
       videoPlayer.play()
+      // change the play button icons to reflect that
       playerPlay.classList.remove('btn__play--play')
       playerPlay.classList.add('btn__play--pause')
     } else {
+      // else pause it
       videoPlayer.pause()
+      // and again change the button icons
       playerPlay.classList.remove('btn__play--pause')
       playerPlay.classList.add('btn__play--play')
     }
@@ -40,26 +30,69 @@
 
   // play & pause click event on play button
   playerPlay.addEventListener('click', () => {
+    // call playVideo on play button click
     playVideo()
   }, false)
 
   // play & pause click event on player
   videoPlayer.addEventListener('click', () => {
+    // call playVideo function on player click
     playVideo()
   }, false)
 
   // restart click event
   playerRestart.addEventListener('click', () => {
+    // restart the video
     videoPlayer.currentTime = 0
   }, false)
 
   // rewind event
   playerRewind.addEventListener('click', () => {
+    // go back ten seconds
     videoPlayer.currentTime -= 10
   }, false)
 
   // fast forward event
   playerForward.addEventListener('click', () => {
+    // go forward ten seconds
     videoPlayer.currentTime += 10
   }, false)
+
+  // mute event
+  playerMute.addEventListener('click', () => {
+    if (videoPlayer.muted === false) {
+      videoPlayer.muted = true
+      // remove & add classes for button icon
+      playerMute.classList.remove('btn__mute--nonmuted')
+      playerMute.classList.add('btn__mute--muted')
+    } else {
+      videoPlayer.muted = false
+      playerMute.classList.remove('btn__mute--muted')
+      playerMute.classList.add('btn__mute--unmuted')
+    }
+  }, false)
+
+  // seek bar event listener (change)
+  playerBar.addEventListener('change', () => {
+    // calculate time
+    let time = videoPlayer.duration * (playerBar.value / 100)
+    // update the time on video
+    videoPlayer.currentTime = time
+  }, false)
+
+  // seek bar event listener (as video plays)
+  videoPlayer.addEventListener('timeupdate', () => {
+    // calculate slider value
+    let value = (100 / videoPlayer.duration) * videoPlayer.currentTime
+    // set slider to value (current video time)
+    playerBar.value = value
+  }, false)
+
+  // skipping while seeking fix
+  playerBar.addEventListener('mousedown', () => {
+    videoPlayer.pause()
+  })
+  playerBar.addEventListener('mouseup', () => {
+    videoPlayer.play()
+  })
 }())
